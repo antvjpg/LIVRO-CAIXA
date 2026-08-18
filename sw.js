79f278d0004013ecf5bfa7d2b0e6268f7942056f
ant.j.jpg <pinheirovinicius928@gmail.com>
2026-08-18 11:53:59 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Livro-Caixa PWA: cacheia a interface local, mas nunca dados financeiros remotos. */
-const CACHE_NAME = "livro-caixa-shell-v4";
+const CACHE_NAME = "livro-caixa-shell-v5";
 const APP_SHELL = [
   "./",
   "./index.html",
